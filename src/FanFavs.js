7ba0dcd7c88fcc14705b4156890eb60a86b619a5
spethstation <114708141+spethstation@users.noreply.github.com>
2023-02-26 15:45:53 -0400

    Added image styling, Splide to create carousel

--- src/FanFavs.js
+++ src/FanFavs.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import "./FanFavs.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 export default function FanFavs() {
   const [random, setRandom] = useState([]);
@@ -10,7 +13,7 @@ export default function FanFavs() {
   async function getRandom() {
     let apiKey = "ad878f3ec4f34864845165807faf5ef2";
     let apiUrl = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=5`
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=8`
     );
     let data = await apiUrl.json();
     setRandom(data.recipes);
@@ -19,13 +22,18 @@ export default function FanFavs() {
 
   return (
     <div>
-      {random.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <Splide>
+        {random.map((recipe) => {
+          return (
+            <SplideSlide>
+              <div key={recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </div>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
     </div>
   );
 }
