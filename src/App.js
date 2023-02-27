e5fbacaf683484040992eb52f9bc6f61dd2822bd
spethstation <114708141+spethstation@users.noreply.github.com>
2023-02-27 17:01:45 -0400

    Adj. slide appearance

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>What's Cooking?</h1>
+      <h3>Explore some of our popular recipes</h3>
       <Main />
     </div>
   );
--- src/FanFavs.js
+++ src/FanFavs.js
@@ -11,21 +11,40 @@ export default function FanFavs() {
   }, []);
 
   async function getRandom() {
-    let apiKey = "ad878f3ec4f34864845165807faf5ef2";
-    let apiUrl = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=8`
-    );
-    let data = await apiUrl.json();
-    setRandom(data.recipes);
-    console.log(data);
+    const checkStorage = localStorage.getItem("random");
+
+    if (checkStorage) {
+      setRandom(JSON.parse(checkStorage));
+    } else {
+      let apiKey = "ad878f3ec4f34864845165807faf5ef2";
+      let apiUrl = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=15`
+      );
+      let data = await apiUrl.json();
+      localStorage.setItem("random", JSON.stringify(data.recipes));
+      setRandom(data.recipes);
+      console.log(data);
+    }
   }
 
   return (
     <div>
-      <Splide>
+      <Splide
+        options={{
+          perPage: 5,
+          perMove: 5,
+          gap: "1em",
+          start: 0,
+          arrows: false,
+          drag: "free",
+          rewind: true,
+          height: "21em",
+          pagination: "splide__pagination your-class-pagination",
+        }}
+      >
         {random.map((recipe) => {
           return (
-            <SplideSlide>
+            <SplideSlide key={recipe.id}>
               <div key={recipe.id}>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
