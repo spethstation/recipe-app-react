3f12caf1655b141c26613b05624101b0b8db869f
spethstation <114708141+spethstation@users.noreply.github.com>
2023-03-01 11:13:00 -0400

    Added cuisine link function

--- src/Cuisines.js
+++ src/Cuisines.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
 
 export default function Cuisines() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
   async function getCuisine(name) {
     let apiKey = "ad878f3ec4f34864845165807faf5ef2";
     const data = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=15&cuisine=${name}`
     );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
   }
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
 
   return <div className="Cuisines"></div>;
 }
